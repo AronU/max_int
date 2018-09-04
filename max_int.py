49c32ba44787c7304a41b147069abd796eebd749
AronU <aronu97@hotmail.com>
2018-09-04 14:02:27 +0000

    Algorithm started for max_int

--- max_int.py
+++ max_int.py
@@ -2,3 +2,18 @@
 #Tekur inn input. 
 #Ef inputtið er stærra en hæsta inputtið(max_int, byrjar í 0) þá er inputtið sett 
 #inn í max_int. 
+
+max_int = 0
+
+
+while max_int >= 0:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int < 0:
+        break
+    elif num_int > max_int:
+        max_int = num_int
+
+
+
+
+print("The maximum is", max_int)    # Do not change this line
