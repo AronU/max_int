13f94559ae9c86481c89ba04db5dc77b0336eaab
AronU <aronu97@hotmail.com>
2018-09-04 14:23:26 +0000

    Algorithm written up in text for sequence.py and skeleton code written

---
+++ sequence.py
@@ -0,0 +1,12 @@
+#for loopa sem stjórnar því hversu oft það keyrist(notar n inputtið)
+#þrjár breytur, tala 1, 2 og 3. Tala 1 er tala 2 + 3 og síðan færist tala 1
+#í tölu 2 og tala 2 færist í 3 og 3 hverfur. 
+
+int_1 = 1
+int_2 = 0
+int_3 = 0
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+    
+for x in range(0, n):
+    print(x)
